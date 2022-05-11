87f3713e6c344f1f86133e701aa773858e83cd84
camila <camilaczeg@gmail.com>
2022-05-11 20:48:24 -0300

    feat(productsService.test.js): add tests for function findProduct

--- test/unit/services/productsService.test.js
+++ test/unit/services/productsService.test.js
@@ -33,10 +33,10 @@ describe('ProductsService', () => {
     });
 
     describe('quando há produtos cadastrados no banco', () => {
-      before(() => {
+      beforeEach(() => {
         sinon.stub(productsModel, 'getAll').resolves(oneResult);
       })
-      after(() => {
+      afterEach(() => {
         productsModel.getAll.restore();
       })
   
@@ -55,6 +55,45 @@ describe('ProductsService', () => {
       it('os objetos têm as propriedades: id, name, quantity', async () => {
         const [data] = await productsService.getAll();
         expect(data).to.have.all.keys('id', 'name', 'quantity');
+      })
+    });
+  });
+
+  describe('A função "findProduct" recebe um id', () => {
+    describe('quando existe um produto com id buscado', () => {
+      beforeEach(() => {
+        sinon.stub(productsModel, 'findProduct').resolves(oneResult);
+      })
+      afterEach(() => {
+        productsModel.findProduct.restore();
+      })
+  
+      it('retorna um objeto', async () => {
+        const data = await productsService.findProduct();
+        expect(data).to.be.an('object');
+      })
+  
+      it('o objeto têm as propriedades: id, name, quantity', async () => {
+        const data = await productsService.findProduct();
+        expect(data).to.have.all.keys('id', 'name', 'quantity');
+      })
+    });
+  
+    describe('quando não há nenhum produto com o id especificado', () => {
+      beforeEach(() => {
+        sinon.stub(productsModel, 'findProduct').resolves(noResults);
+      })
+      afterEach(() => {
+        productsModel.findProduct.restore();
+      })
+  
+      const error = { status: 404, message: 'Product not found' };
+      it('gera o erro: "Product not found"', async () => {
+        try {
+          await productsService.findProduct();
+        } catch(err) {
+          expect(err).to.eql(error);
+        }
       })
     });
   });
